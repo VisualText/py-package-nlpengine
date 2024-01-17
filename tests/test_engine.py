42aedb81c2f985afba13879ec3e09dc525809201
David Huggins-Daines <dhd@ecolingui.ca>
2024-01-17 15:52:45 +0000

    tests: test the little analyzers

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -5,27 +5,62 @@ Test the basic engine functionality.
 from unittest import TestCase, main
 from pathlib import Path
 
+import json
 import logging
 
 import NLPPlus
 
 DATADIR = Path(__file__).parent / "data"
 
+def read_file(path):
+    with open(path, "rt") as infh:
+        return infh.read()
+
 
 class EngineTest(TestCase):
     """Test the NLPPlus library"""
 
+    maxDiff = None
+
     def test_basic(self):
-        """Run the "basic" analyzer and verify that it creates some output."""
+        """Run the "basic" analyzer and verify that it works."""
 
         output = NLPPlus.analyze("Hello world!", "basic")
         # This analyzer does not produce any output :)
         self.assertEqual(output, "")
-        # Here's a better API ;-)
-        results = NLPPlus.engine.analyze("Hello world!", "basic")
-        with open(DATADIR / "basic" / "output" / "final.tree") as infh:
-            final_tree = infh.read()
-        self.assertEqual(results.final_tree, final_tree)
+        # Here's a better API!
+        text = read_file(DATADIR / "basic" / "text.txt")
+        results = NLPPlus.engine.analyze(text, "basic")
+        self.assertEqual(results.output_text, "")
+        final_tree = read_file(DATADIR / "basic" / "text.txt_log" / "final.tree")
+        self.assertEqual(final_tree, results.final_tree)
+
+    # FIXME: we have to do this to get them in separate tests, but
+    # with pytest we could do a parameterized test case.
+    def _run_analyzer(self, name):
+        text = read_file(DATADIR / name / "text.txt")
+        results = NLPPlus.engine.analyze(text, name)
+        self.assertEqual(results.output_text, "")
+        output = json.loads(
+            read_file(DATADIR / name / "text.txt_log" / "output.json")
+        )
+        self.assertEqual(output, results.output)
+
+    def test_address_parser(self):
+        """Run the address parser and verify that it works."""
+        self._run_analyzer("address-parser")
+
+    def test_emailaddress_parser(self):
+        """Run the emailaddress analyzer and verify that it works."""
+        self._run_analyzer("emailaddress")
+
+    def test_telephone_parser(self):
+        """Run the telephone analyzer and verify that it works."""
+        self._run_analyzer("telephone")
+
+    def test_links_parser(self):
+        """Run the links analyzer and verify that it works."""
+        self._run_analyzer("links")
 
 
 if __name__ == "__main__":
